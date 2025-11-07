48b87a525baf89f03a5843a28d200db8b8e65cea
temurqobilov1 <qobilovtemuruz@gmail.com>
2025-11-07 14:59:34 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function App() {
   return <div>
     <a href="https://github.com/temurqobilov1/dashboard_crm">https://github.com/temurqobilov1/dashboard_crm</a>
+    <p>Assalomu alaykum ustoz erpga vaqt tugab ketmasin deb link joylab qoygandim. <br /> Ustoz kecha darsda yozgan kodlarni birga bir kochirdim lekin baribir menda ishlamayapti API da xato chiqyapti</p>
   </div>;
 }
 
